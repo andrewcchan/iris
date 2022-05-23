3e8ea9203854e3b4c10d953b1e1943aa5b31bd8a
andrewcchan <40149441+andrewcchan@users.noreply.github.com>
2022-05-23 15:04:22 -0700

    feat: add iris svm trained  model

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,13 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
+from joblib import dump, load
 
 # APP
 
 st.title('Iris Dataset')
+'''https://colab.research.google.com/drive/10O1KYBqC_Cw1lMTbS2x4bCSRTsEdS7_r#scrollTo=hIOxoY8S7dE2'''
+
+clf = load('./iris.joblib')
+testRes = clf.predict([[-0.8, -1]])
+testRes
